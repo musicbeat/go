8094f7fcae59d5ccc51a6df292c3b18a8a3805d1
Craig Thomas <craig.a.thomas@gmail.com>
2014-03-05 19:48:16 -0800

    asyncpoc is demo-able

--- asyncpoc/asyncpoc.go
+++ asyncpoc/asyncpoc.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/musicbeat/worker"
+	"github.com/musicbeat/go/worker"
 	"log"
 	"net/http"
+	"time"
 )
 
 type ClientRequest struct {
@@ -14,7 +15,27 @@ type ClientRequest struct {
 	RequestState   string
 }
 
+const (
+	stepCount  = 3
+	sleepCount = 10
+	sleepTime  = time.Millisecond * 10
+)
+
+/*
+ServeHTTP represents the processing service. It implements the
+series of business logic required to service the ClientRequest.
+The business logic consists of performing the same step stepCount
+times.  Each time the step is performed it has a randomly selected
+latency.  The business logic steps are placed on a queue. worker.Work()
+performs them in succession. ServeHTTP sleeps for 10ms, checks to
+see if everything is done. If not, it sleeps for another 10ms. If,
+after 10 iterations of 10ms sleeping (100ms or so), it declares
+itself done enough.
+*/
 func (req ClientRequest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	start := time.Now()
+
 	// prepare the processing order:
 	po := worker.ProcessingOrder{
 		Request: worker.ProcessingRequest{
@@ -24,11 +45,32 @@ func (req ClientRequest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		},
 		Response: []worker.ProcessingResponse{},
 	}
-	c := make(chan int)
-	// just execute a work function:
-	go worker.Work(&po, c)
-	// wait for it:
-	<- c
+
+	queue := make(chan *worker.Step, stepCount)
+
+	// set up the multi-step business process:
+	var bp []worker.Step = make([]worker.Step, stepCount)
+	for i := 0; i < stepCount; i++ {
+		bp[i] = worker.Step{po.Request, worker.Work, make(chan worker.ProcessingResponse)}
+		queue <- &bp[i]
+	}
+	// put the worker to work:
+	go worker.Handle(queue)
+
+	// collect the responses, but don't wait too long:
+	c := make(chan uint)
+	go collect(bp, &po, c, start)
+	bits := <- c
+	if bits == 0 {
+		// note that the request is completed:
+		po.Request.RequestState = "complete"
+	} else {
+		po.Request.RequestState = "in progress"
+	}
+
+	log.Printf("elapsed: %03d\n", time.Since(start).Nanoseconds()/1e6)
+
+	// jsonify:
 	j, err := json.MarshalIndent(po, "", "  ")
 	if err == nil {
 		fmt.Fprint(w, fmt.Sprintf("%s\n", j))
@@ -37,6 +79,47 @@ func (req ClientRequest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func collect(bp []worker.Step, po *worker.ProcessingOrder, c chan uint, start time.Time) {
+	// keep track in a bitmap:
+	var bits, i uint
+	for i = 0; i < stepCount; i++ {
+		bits = bits | 1 << i
+	}
+	for i := 0; i < sleepCount; i++ {
+		// sleep
+		<-time.NewTimer(sleepTime).C
+		// log.Printf("collect elapsed: %03d\n", time.Since(start).Nanoseconds()/1e6)
+		// check each step in the business process:
+		var j uint
+		for j = 0; j < stepCount; j++ {
+			// do a non-blocking read on the channel
+			select {
+			case pr, found := <-bp[j].ResultChan:
+				// if there's a response, append it to the processing order's responses
+				if found {
+					po.Response = append(po.Response, pr)
+					// clear this step's bit:
+					bits = bits ^ 1 << j
+					log.Printf("poll %d; chan %d; bits: %b\n", i, j, bits)
+				}
+			default:
+				// log.Printf("poll %d; chan %d; nothing yet\n", i, j)
+			}
+		}
+		if bits == 0 {
+			break
+		}
+	}
+	// log.Printf("collect elapsed: %03d\n", time.Since(start).Nanoseconds()/1e6)
+	c <- bits
+}
+
+/*
+main simulates the client. It looks like it's waiting for a
+client to present a request. But for this simulation, it
+fakes the ClientRequest itself. It waits for a client to do
+a get on the /process resource.
+*/
 func main() {
 	r := ClientRequest{
 		RequestId:      "1001",
--- worker/worker.go
+++ worker/worker.go
@@ -10,9 +10,13 @@ package worker
 import (
 	"code.google.com/p/go-uuid/uuid"
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
+
+// TODO: this is just to silence compiler's complaints that there's no logging below:
+var _ = log.Printf
 
 /*
 ProcessingRequest tells the worker what to do. The client or service
@@ -51,22 +55,43 @@ type ProcessingOrder struct {
 	Response []ProcessingResponse
 }
 
-var durations = []int{5, 50, 500}
+/*
+Step is the work that worker needs to do. worker.Work waits for a
+Step to be added to its queue. then it does the work.
+*/
+type Step struct {
+	Request    ProcessingRequest
+	Task       func(ProcessingRequest) ProcessingResponse
+	ResultChan chan ProcessingResponse
+}
+
+var durations = []time.Duration{
+	5 * time.Millisecond,
+	50 * time.Millisecond,
+	500 * time.Millisecond,
+}
 
 /*
 Work performs the work simulation and returns the result in ProcessingResponse.
 */
-func Work(po *ProcessingOrder, c chan int) {
+func Work(req ProcessingRequest) ProcessingResponse {
 	rand.Seed(time.Now().UnixNano())
 	d := durations[rand.Intn(len(durations))]
-	time.Sleep(time.Duration(d) * time.Millisecond)
+	time.Sleep(d)
 	uuid := uuid.NewRandom()
-	resp := ProcessingResponse{
+	return ProcessingResponse{
 		ResponseId:      uuid.String(),
-		ResponseContent: fmt.Sprintf("slept for %dms", d),
+		ResponseContent: fmt.Sprintf("slept for %dms", d/time.Millisecond),
 		ResponseState:   "done",
 	}
-	po.Response = append(po.Response, resp)
-	c <- 1
-	return
+}
+
+/*
+handle() dispatches the queued work.
+*/
+func Handle(queue chan *Step) {
+	for req := range queue {
+		req.ResultChan <- req.Task(req.Request)
+		close(req.ResultChan)
+	}
 }
